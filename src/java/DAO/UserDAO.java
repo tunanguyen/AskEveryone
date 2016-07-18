97f832ec5618d598fb3f1dc6d763476e5510bad5
Nguyễn Minh Ngọc <ngocnmse03412.fpt@gmail.com>
2016-07-18 17:40:13 +0700

    Coding UserDAO

--- src/java/DAO/UserDAO.java
+++ src/java/DAO/UserDAO.java
@@ -9,6 +9,8 @@ import entity.User;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,10 +18,20 @@ import java.sql.SQLException;
  */
 public class UserDAO extends AbstractDAO<User> {
 
+    private static final UserDAO INSTANCE = new UserDAO();
     private static final String USERNAME = "userName";
     private static final String PASSWORD = "password";
     private static final String EMAIL = "email";
     String sqlStatement = null;
+
+    private UserDAO() {
+
+    }
+
+    public static UserDAO getInstance() {
+        return INSTANCE;
+    }
+
     @Override
     protected User castFromDB(ResultSet record) {
         if (record == null) {
@@ -35,9 +47,9 @@ public class UserDAO extends AbstractDAO<User> {
         }
         return user;
     }
-    
-    public User checkLogin(User user) throws SQLException{
-        sqlStatement = "SELECT * FROM Users WHERE userName = ? AND password = ?";
+
+    public User getAccount(User user) throws SQLException {
+        sqlStatement = "SELECT * FROM USERS WHERE USERNAME = ? AND PASSWORD = ?";
         User rsUser = null;
         PreparedStatement ps = getConnection().prepareStatement(sqlStatement);
         ps.setString(1, user.getUserName());
@@ -48,10 +60,52 @@ public class UserDAO extends AbstractDAO<User> {
         }
         return rsUser;
     }
-    public static void main(String[] args) throws SQLException {
-        UserDAO userDAO = new UserDAO();
-        User user = new User();
-        user = userDAO.checkLogin(new User("admin", "admin", ""));
-        System.out.println(user.getUserName() + "_____________");
+
+    public boolean createAccount(User user) {
+        sqlStatement = "INSERT INTO USERS(USERNAME,PASSWORD,EMAIL) VALUES (?,?,?)";
+        PreparedStatement ps;
+        try {
+            ps = getConnection().prepareStatement(sqlStatement);
+            ps.setString(1, user.getUserName());
+            ps.setString(2, user.getPassword());
+            ps.setString(3, user.getEmail());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    public boolean cheackUsernameExist(String username) throws SQLException {
+        sqlStatement = "SELECT * FROM USERS WHERE USERNAME = ?";
+        PreparedStatement ps = getConnection().prepareStatement(sqlStatement);
+        ps.setString(1, username);
+        ResultSet rs = ps.executeQuery();
+        int check = 0;
+        while (rs.next()) {
+            check++;
+        }
+        if (check == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean cheackEmailExist(String email) throws SQLException {
+        sqlStatement = "SELECT * FROM USERS WHERE EMAIL = ?";
+        PreparedStatement ps = getConnection().prepareStatement(sqlStatement);
+        ps.setString(1, email);
+        ResultSet rs = ps.executeQuery();
+        int check = 0;
+        while (rs.next()) {
+            check++;
+        }
+        if (check == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
